421abb1dc0a2d8493a694568e680cb4a273030b5
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2022-04-02 12:59:49 -0300

    funcionamiento episodios

--- src/components/TarjetaEpisodio.js
+++ src/components/TarjetaEpisodio.js
@@ -1,9 +1,23 @@
-const TarjetaEpisodio = () => {
-    return (
-        <div>
-            <h2>Tarjeta episodio</h2>
-        </div>
-    )
-}
+const TarjetaEpisodio = (
+ {temporada,
+  episodio,
+  nombre,
+  resumen,
+  imagen,
+  id}
+) => {
+  return (
+    <div key={id} className="tarjeta-pisodio">
+      <div>
+        <img className="imagen-episodio" src={imagen} />
+      </div>
+      <h3>
+        S{temporada}E{episodio}
+      </h3>
+      <h3>{nombre}</h3>
+      <p>{resumen}</p>
+    </div>
+  );
+};
 
-export default TarjetaEpisodio
+export default TarjetaEpisodio;
--- src/components/Temporadas.js
+++ src/components/Temporadas.js
@@ -2,22 +2,27 @@ import useFetchTemporadas from "../hooks/useFetchTemporadas";
 import useFetchIndividual from "../hooks/useFetchIndividual";
 import LinksDetalle from "./LinksDetalle";
 import HeaderDetalle from "./HeaderDetalle";
+import TarjetaEpisodio from "./TarjetaEpisodio";
 import { useParams, Link } from "react-router-dom";
 import { selectTemporadas } from "../auxiliares/funciones";
-import { useState } from "react"
+import { useState } from "react";
 
 const Temporadas = () => {
   const params = useParams();
   const [temporadaSeleccionada, setTemporadaSeleccionada] = useState(1);
-  const temporadas = useFetchTemporadas(params.categoria, params.id, temporadaSeleccionada);
+  const temporadas = useFetchTemporadas(
+    params.categoria,
+    params.id,
+    temporadaSeleccionada
+  );
   const numeroTemporadas = useFetchIndividual(params.categoria, params.id);
-    const episodios = temporadas.episodes 
+  const episodios = temporadas.episodes;
 
-  console.log(temporadas)
+  console.log(temporadas);
 
-    const handleChange = (e) => {
-        setTemporadaSeleccionada(e.target.value)
-    }
+  const handleChange = (e) => {
+    setTemporadaSeleccionada(e.target.value);
+  };
 
   return (
     <div className="temporadas">
@@ -25,12 +30,26 @@ const Temporadas = () => {
       <LinksDetalle />
       <div className="contenedor-select-temporadas">
         <label>
-          <select onChange={handleChange} className="select-temporadas" name="select-temporadas">
+          <select
+            onChange={handleChange}
+            className="select-temporadas"
+            name="select-temporadas"
+          >
             {selectTemporadas(numeroTemporadas?.number_of_seasons)}
           </select>
         </label>
       </div>
-
+      <div className="contenedor-episodios">
+        {episodios?.map((elemento) => (
+          <TarjetaEpisodio
+            temporada={elemento.season_number}
+            episodio={elemento.episode_number}
+            nombre={elemento.name}
+            resumen={elemento.overview}
+            imagen={`https://image.tmdb.org/t/p/w300/${elemento.still_path}`}
+          />
+        ))}
+      </div>
     </div>
   );
 };
