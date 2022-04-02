2d599359b2809050c87dfdc7372549dfd473c7a4
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2022-04-02 12:43:05 -0300

    estilado select

--- src/components/Temporadas.js
+++ src/components/Temporadas.js
@@ -4,30 +4,33 @@ import LinksDetalle from "./LinksDetalle";
 import HeaderDetalle from "./HeaderDetalle";
 import { useParams, Link } from "react-router-dom";
 import { selectTemporadas } from "../auxiliares/funciones";
+import { useState } from "react"
 
 const Temporadas = () => {
   const params = useParams();
-  const temporadas = useFetchTemporadas(params.categoria, params.id, 1);
+  const [temporadaSeleccionada, setTemporadaSeleccionada] = useState(1);
+  const temporadas = useFetchTemporadas(params.categoria, params.id, temporadaSeleccionada);
   const numeroTemporadas = useFetchIndividual(params.categoria, params.id);
+    const episodios = temporadas.episodes 
 
+  console.log(temporadas)
 
-
-  console.log(temporadas);
-  console.log(numeroTemporadas?.number_of_seasons)
-
-
+    const handleChange = (e) => {
+        setTemporadaSeleccionada(e.target.value)
+    }
 
   return (
     <div className="temporadas">
       <HeaderDetalle />
       <LinksDetalle />
-      <div>
+      <div className="contenedor-select-temporadas">
         <label>
-          <select name="select-personajes">
+          <select onChange={handleChange} className="select-temporadas" name="select-temporadas">
             {selectTemporadas(numeroTemporadas?.number_of_seasons)}
           </select>
         </label>
       </div>
+
     </div>
   );
 };
