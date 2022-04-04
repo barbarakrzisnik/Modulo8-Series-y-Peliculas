5f93bf38ad3d8303f6b0b99fec5aa76d1789f532
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2022-04-03 22:34:56 -0300

    seccion busqueda logica completa

--- src/hooks/useFetchBusqueda.js
+++ src/hooks/useFetchBusqueda.js
@@ -8,7 +8,7 @@ const useFetchBusqueda = (busqueda) => {
       fetch(`https://api.themoviedb.org/3/search/multi?api_key=8649accd4d54c4ebc3606e3e22a94d03&language=es-AR&query=${busqueda}&page=1`)
       .then(res=> res.json())
       .then(data => setdatosFetchBusqueda(data.results))
-    }, [])
+    }, [busqueda])
     
     return datosFetchBusqueda
   }
