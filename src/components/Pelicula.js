5dbd034e6dbc5fcb49dac9b7232477643271d956
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2022-04-03 22:41:31 -0300

    armado base peliculas

--- src/components/Pelicula.js
+++ src/components/Pelicula.js
@@ -1,8 +1,18 @@
+import ComponentePreview from "./ComponentePreview";
+import useFetch from "../hooks/useFetch"
 
 const Pelicula = () => {
+    const peliculasPopulares = useFetch("movie", "popular")
+    const peliculasMejoresCriticas = useFetch("movie", "top_rated")
+    const peliculasEstrenar = useFetch("movie", "upcoming")
+    const peliculasCine = useFetch("movie", "now_playing")
+
     return (
         <div>
-            <h1>Peliculas</h1>
+            <ComponentePreview array={peliculasPopulares.slice(0,5)} titulo="Peliculas Populares" categoria="movie"/>
+            <ComponentePreview array={peliculasMejoresCriticas.slice(0,5)} titulo="Peliculas con mejores criticas" categoria="movie"/>
+            <ComponentePreview array={peliculasEstrenar.slice(0,5)} titulo="Peliculas a estrenar" categoria="movie"/>
+            <ComponentePreview array={peliculasCine.slice(0,5)} titulo="Peliculas en el cine" categoria="movie"/>
 
 
         </div>
