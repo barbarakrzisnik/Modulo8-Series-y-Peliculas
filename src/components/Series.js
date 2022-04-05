fa1df3144b215c5dbd3e1b3db02aac6c9b4cfce0
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2022-04-04 23:05:22 -0300

    funcionamiento seccion series

--- src/components/Series.js
+++ src/components/Series.js
@@ -1,6 +1,18 @@
+import ComponentePreview from "./ComponentePreview";
+import useFetch from "../hooks/useFetch"
+
 const Series = () => {
+    const seriesPopulares = useFetch("tv", "popular")
+    const seriesMejoresCriticas = useFetch("tv", "top_rated")
+    const seriesAlAire = useFetch("tv", "airing_today")
+    console.log(seriesAlAire)
+
     return (
-        <h1>Series</h1>
+        <div>
+            <ComponentePreview array={seriesPopulares.slice(0,5)} titulo="Series Populares" categoria="tv" valoracion="popular" />
+            <ComponentePreview array={seriesMejoresCriticas.slice(0,5)} titulo="Series con mejores criticas" categoria="tv" valoracion="top_rated"/>
+            <ComponentePreview array={seriesAlAire.slice(0,5)} titulo="Series al aire" categoria="tv" valoracion="airing_today"/>
+        </div>
     )
 }
 
