7c3ee810995534e1f77339f64595e5da8cde967c
barbarakrzisnik <barbarakrzisnik3@gmail.com>
2022-04-04 22:49:01 -0300

    estilado de links

--- src/components/ComponentePreview.js
+++ src/components/ComponentePreview.js
@@ -1,29 +1,26 @@
 import Tarjetas from "./Tarjetas";
 import { useParams, Link } from "react-router-dom";
-
+import { FaArrowRight } from "react-icons/fa";
 
 const ComponentePreview = ({ array, titulo, categoria, valoracion }) => {
   return (
-
-
     <div className="contenedor-componenete-preview">
-       <Link
-          className="link-detalle"
-          to={`/${categoria}/${valoracion}/pagina/:numeroPagina`}
-        >{titulo}</Link>
-    <div className="contenedor-tarjetas-componente-preview">
-    {array.map((elemento) => (
-        <Tarjetas
-          key={elemento.id}
-          title={elemento.title}
-          name={elemento.name}
-          imagen={`https://image.tmdb.org/t/p/w300/${elemento.poster_path}`}
-          id={elemento.id}
-          categoria={categoria}
-        />
-      ))}
-    </div>
-      
+      <Link
+        className="link-detalle"
+        to={`/${categoria}/${valoracion}/pagina/:numeroPagina`}
+      >{`${titulo}`}< FaArrowRight className="icono" /></Link>
+      <div className="contenedor-tarjetas-componente-preview">
+        {array.map((elemento) => (
+          <Tarjetas
+            key={elemento.id}
+            title={elemento.title}
+            name={elemento.name}
+            imagen={`https://image.tmdb.org/t/p/w300/${elemento.poster_path}`}
+            id={elemento.id}
+            categoria={categoria}
+          />
+        ))}
+      </div>
     </div>
   );
 };
